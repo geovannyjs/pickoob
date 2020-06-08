3b3fefff96fa5094f0f3d02d17a4d2bbec1d521a
Geovanny Junio <geovannyjs@gmail.com>
2020-06-08 10:50:03 -0300

    show that data is a javascript object

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ const myParser = new RdfXmlParser()
 const myTextStream = fs.createReadStream('/tmp/gutenberg/1/pg1.rdf')
 
 myParser.import(myTextStream)
-  .on('data', console.log)
+  .on('data', (d) => console.log(JSON.stringify(d)))
   .on('error', console.error)
   .on('end', () => console.log('All triples were parsed!'))
