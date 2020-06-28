2cafb3912f9f3688216080dae3d5d0f535c36519
Lucas Gabriel <luke3skywalker@gmail.com>
2020-06-28 17:06:45 -0300

    showing examples in all categories

--- http/app.js
+++ http/app.js
@@ -128,21 +128,51 @@ mongo.MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: tru
 
   router.get('/authors', function (req, res) {
     res.statusCode = 200
-    let searchParameter = {name: {$regex: new RegExp(queryStringAsObject(req).search)}}
+    let searchParameter = {name: {$regex: new RegExp(queryStringAsObject(req).search, 'i')}}
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    //queryStringAsObject(req)
     if(searchParameter){
-      buildPaging(authorColl, req, searchParameter).then(paging => {
-        authorColl.find(searchParameter).skip(paging.skip).limit(paging.limit).toArray()
-          .then(authors => res.end(authorList({ authors, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
-      })
 
+      buildPaging(authorColl, req, searchParameter).then(paging => {
+        //Getting an array with the results of author's table consult with a search parameter
+        authorColl.find(searchParameter).skip(paging.skip).limit(paging.limit).toArray()
+        .then(authors => authors.map(x => {
+          /*for each element from the array we do a search on book's table, to get the books where 
+          the author's id field from books is equal to the id from author's table current element*/
+          return bookColl.find({author: {$eq: x._id}}).limit(5).toArray()
+          /*then we transform the books and the authors that we got into a new object where each
+           element contains the author's information and the books that he wrote*/
+            .then(bookPromise => {return{
+              author: x,
+              books: bookPromise
+            }}
+            )
+        }))
+        //we need to wait until all the results are ready  
+        .then((x) => Promise.all(x))
+        //pass the parameters to authorList to create the html page
+        .then(object => res.end(authorList({ object, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
+      })
     }
+
     else{
+      /*same as the if statement, the difference here is that when we call the authorList function
+      we don't pass search as a parameter*/
       buildPaging(authorColl, req).then(paging => {
         authorColl.find({}).skip(paging.skip).limit(paging.limit).toArray()
-          .then(authors => res.end(authorList({ authors, paging })))
+        .then(authors => authors.map(x => {
+          return bookColl.find({author: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              author: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(authorList({ object, paging })))
       })
+
+
+
     }
     console.log(queryStringAsObject(req).search)
     //res.statusCode = 200
@@ -190,14 +220,37 @@ mongo.MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: tru
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     let searchParameter = {code: {$regex: new RegExp(queryStringAsObject(req).search, 'i')}}
     if(searchParameter){
+      //same as author
       buildPaging(languageColl, req, searchParameter).then(paging => {
         languageColl.find(searchParameter).skip(paging.skip).limit(paging.limit).toArray()
-          .then(languages => res.end(languageList({ languages, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
+        .then(languages => languages.map(x => {
+          return bookColl.find({language: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              language: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(languageList({ object, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
       })
     }
-    else{
-      languageColl.find({}).skip(paging.skip).limit(paging.limit).toArray()
-      .then(languages => res.end(languageList({ languages, paging })))
+    else{    
+      //same as author
+      buildPaging(languageColl, req).then(paging => {
+        languageColl.find({}).skip(paging.skip).limit(paging.limit).toArray()
+        .then(language => language.map(x => {
+          return bookColl.find({language: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              language: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(languageList({ object, paging })))
+      })
+    
     }
   })
 
@@ -212,23 +265,41 @@ mongo.MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: tru
     res.statusCode = 200
     let searchParameter = {name: {$regex: new RegExp(queryStringAsObject(req).search, 'i')}}
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    //queryStringAsObject(req)
     if(searchParameter){
+      //same as author
       buildPaging(shelfColl, req, searchParameter).then(paging => {
         shelfColl.find(searchParameter).skip(paging.skip).limit(paging.limit).toArray()
-          .then(shelves => res.end(shelfList({ shelves, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
+        .then(shelves => shelves.map(x => {
+          return bookColl.find({shelf: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              shelf: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(shelfList({ object, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
       })
 
     }
     else{
+      //same as author
       buildPaging(shelfColl, req).then(paging => {
         shelfColl.find({}).skip(paging.skip).limit(paging.limit).toArray()
-          .then(shelves => res.end(shelfList({ shelves, paging })))
+        .then(shelves => shelves.map(x => {
+          return bookColl.find({shelf: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              shelf: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(shelfList({ object, paging })))
       })
+
     }
     console.log(queryStringAsObject(req).search)
-    //res.statusCode = 200
-    //res.setHeader('Content-Type', 'text/html; charset=utf-8')
   })
 
   router.get('/shelf/:title/:id', function (req, res) {
@@ -243,6 +314,7 @@ mongo.MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: tru
     let searchParameter = {name: {$regex: new RegExp(queryStringAsObject(req).search, 'i')}}
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     if(searchParameter){
+      //same as author
       buildPaging(subjectColl, req, searchParameter).then(paging => {
         subjectColl.find(searchParameter).skip(paging.skip).limit(paging.limit).toArray()
         .then(subjects => subjects.map(x => {
@@ -254,14 +326,23 @@ mongo.MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: tru
             )
         }))  
         .then((x) => Promise.all(x))
-        //.then(() => res.end('aa'))
         .then(object => res.end(subjectList({ object, paging, search:((queryStringAsObject(req).search) ? queryStringAsObject(req).search : 'invERRORalid') })))
       })
     }
     else{
+      //same as author
       buildPaging(subjectColl, req).then(paging => {
         subjectColl.find({}).skip(paging.skip).limit(paging.limit).toArray()
-          .then(subjects => res.end(subjectList({ subjects, paging })))
+        .then(subjects => subjects.map(x => {
+          return bookColl.find({subject: {$eq: x._id}}).limit(5).toArray()
+            .then(bookPromise => {return{
+              subject: x,
+              books: bookPromise
+            }}
+            )
+        }))  
+        .then((x) => Promise.all(x))
+        .then(object => res.end(subjectList({ object, paging })))
       })
     }
   })
--- http/templates/shelf/list.js
+++ http/templates/shelf/list.js
@@ -2,20 +2,20 @@ const sanitize = require('../../../lib/string/sanitize')
 
 const wrapper = require('../components/wrapper')
 const paging = require('../components/paging')
-
+const listExamples = require('../book/listExamples')
 
 const list = (p) => {
 
   if(p.search == 'invERRORalid'){
-  let content = p.shelves
-    .map(x => `<a href="/shelf/${sanitize(x.name)}/${x._id}">${x.name}</a><br>`).join('') + 
+  let content = p.object
+    .map(x => `<a href="/subject/${sanitize(x.shelf.name)}/${x.shelf._id}">${x.shelf.name}</a><br><br> ${listExamples(x.books)} <br><br> `).join('') + 
     '<br><br>' + paging(p.paging, (page) => `/shelves?page=${page}`)
 
     return wrapper({ content })
   }
   else{
-    let content = p.shelves
-    .map(x => `<a href="/shelf/${sanitize(x.name)}/${x._id}">${x.name}</a><br>`).join('') + 
+    let content = p.object
+    .map(x => `<a href="/subject/${sanitize(x.shelf.name)}/${x.shelf._id}">${x.shelf.name}</a><br><br> ${listExamples(x.books)} <br><br> `).join('') + 
     '<br><br>' + paging(p.paging, (page) => `/shelves?page=${page}&search=${p.search}`)
 
   return wrapper({ content })    
