32597fa4a6e494cc5ad1773bd1910379134ef8bb
Lucas Gabriel <luke3skywalker@gmail.com>
2020-06-28 20:31:42 -0300

    links added

--- http/templates/book/listExamples.js
+++ http/templates/book/listExamples.js
@@ -1,9 +1,12 @@
 
+const sanitize = require('../../../lib/string/sanitize')
+
+
 //receive an array of books
 const listExamples = (books) => {
     let bookString = ''
     books.forEach(x => {
-        bookString = bookString + `<p>${x.title}</p>` //concatenating the titles to return the HTML string
+        bookString = bookString + `<a href="/book/${sanitize(x.title)}/${x._id}">${x.title}</a><br>` //concatenating the titles to return the HTML string
     })
     return bookString
 }
