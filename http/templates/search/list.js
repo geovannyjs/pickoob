c6aa6e3d95329ca06356ce36e76a34547149fae9
Geovanny Junio <geovannyjs@gmail.com>
2020-07-06 16:05:46 -0300

    http search titles

--- http/templates/search/list.js
+++ http/templates/search/list.js
@@ -3,11 +3,11 @@ const wrapper = require('../components/wrapper')
 
 const list = (p) => {
 
-  let content = p.books.map(x => `<a href="/book/${x.unique}/${x._id}">${x.title}</a><br>`).join('') + '<br><br>' +
-    p.subjects.map(x => `<a href="/subject/${x.unique}/${x._id}">${x.name}</a><br>`).join('') + '<br><br>' +
-    p.authors.map(x => `<a href="/author/${x.unique}/${x._id}">${x.name}</a><br>`).join('') + '<br><br>' +
-    p.languages.map(x => `<a href="/language/${x.code}/${x._id}">${x.code}</a><br>`).join('') + '<br><br>' +
-    p.shelves.map(x => `<a href="/shelf/${x.unique}/${x._id}">${x.name}</a><br>`).join('')
+  let content = p.books.map(x => `<a href="/book/${x.unique}/${x._id}" title="${x.title}">${x.title}</a><br>`).join('') + '<br><br>' +
+    p.subjects.map(x => `<a href="/subject/${x.unique}/${x._id}" title="${x.name}">${x.name}</a><br>`).join('') + '<br><br>' +
+    p.authors.map(x => `<a href="/author/${x.unique}/${x._id}" title="${x.name}">${x.name}</a><br>`).join('') + '<br><br>' +
+    p.languages.map(x => `<a href="/language/${x.code}/${x._id}" title="${x.code}">${x.code}</a><br>`).join('') + '<br><br>' +
+    p.shelves.map(x => `<a href="/shelf/${x.unique}/${x._id}" title="${x.name}">${x.name}</a><br>`).join('')
   
   return wrapper({ content })
 
